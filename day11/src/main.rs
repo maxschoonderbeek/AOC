aa41ca56731e57d517b1f993d930ef6b19189d4d
MaxSchoonderbeek <maxschoonderbeek@outlook.com>
2021-04-09 15:39:29 +0200

    day11_part2_finished

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,5 +1,5 @@
 
-use std::{fs, ptr::read_unaligned};
+use std::{fs};
 
 const _RAW_INP1: &str = r"L.LL.LL.LL
 LLLLLLL.LL
@@ -13,9 +13,9 @@ L.LLLLLL.L
 L.LLLLL.LL";
 
 fn main() {
-    // let input = fs::read_to_string("src/input.txt").expect("Something went wrong reading the file");
+    let input = fs::read_to_string("src/input.txt").expect("Something went wrong reading the file");
     
-    let input = _RAW_INP1.to_string();
+    // let input = _RAW_INP1.to_string();
     
     let seats: Vec<_> = input
         .chars()
@@ -42,13 +42,13 @@ fn main() {
                 if seats[index] {
                     if occupied[index] == 1
                     {
-                        if count_neighbours(&occupied, row, col, row_len, col_len) >= 4 {
+                        if count_neighbours(&occupied, col, row, row_len, col_len) >= 4 {
                             occupied_new[index] = 0;
                         }  
                     }
                     else
                     {
-                        if count_neighbours(&occupied, row, col, row_len, col_len) == 0 {
+                        if count_neighbours(&occupied, col, row, row_len, col_len) == 0 {
                             occupied_new[index] = 1;
                         }
                     }
@@ -65,6 +65,14 @@ fn main() {
     //println!("seat occupation = {:?}", occupied);
     println!("count = {}", occupied.iter().filter(|&i|*i == 1).count());
 
+    // -------------
+    // Puzzle 2
+    // -------------
+    // reset occupancy
+    occupied_new = vec![0;seats.len()];
+    occupied = vec![0;seats.len()];
+    stable = false;
+    run_id = 0;
     while !stable {
         for col in 0..col_len{
             for row in 0..row_len{
@@ -72,13 +80,13 @@ fn main() {
                 if seats[index] {
                     if occupied[index] == 1
                     {
-                        if count_neighbours_in_sight(&occupied, row, col, row_len, col_len) >= 4 {
+                        if count_neighbours_in_sight(&seats, &occupied, col, row, row_len, col_len) >= 5 {
                             occupied_new[index] = 0;
                         }  
                     }
                     else
                     {
-                        if count_neighbours_in_sight(&occupied, row, col, row_len, col_len) == 0 {
+                        if count_neighbours_in_sight(&seats, &occupied, col, row, row_len, col_len) == 0 {
                             occupied_new[index] = 1;
                         }
                     }
@@ -95,7 +103,7 @@ fn main() {
     println!("count = {}", occupied.iter().filter(|&i|*i == 1).count());
 }
 
-fn count_neighbours(occupied:&Vec<isize>, row:usize, col:usize, row_len:usize, col_len:usize) -> isize
+fn count_neighbours(occupied:&Vec<isize>, col:usize, row:usize, row_len:usize, col_len:usize) -> isize
 {
     let mut neighbour_count=0;
     let row = row as isize;
@@ -114,36 +122,42 @@ fn count_neighbours(occupied:&Vec<isize>, row:usize, col:usize, row_len:usize, c
     return neighbour_count;
 }
 
-fn count_neighbours_in_sight(seat:&Vec<bool>, occupied:&Vec<isize>, row:usize, col:usize, row_len:usize, col_len:usize) -> isize
+fn count_neighbours_in_sight(seats:&Vec<bool>, occupied:&Vec<isize>, col:usize, row:usize, row_len:usize, col_len:usize) -> isize
 {
     let mut neighbour_count=0;
     // create clockwise directions (col,row)
     let direction_tuple: [(isize,isize);8] = [(-1,-1), (0,-1), (1,-1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)];
-    for direction in direction_tuple.chunks(1){
-        neighbour_count += search_in_direction(seat, occupied, row, col, row_len, col_len, direction);
+    for direction in direction_tuple.iter(){
+        neighbour_count += search_in_direction(seats, occupied, col, row, row_len, col_len, direction);
     }
     return neighbour_count;
 }
 
-fn search_in_direction(seat:&Vec<bool>, occupied:&Vec<isize>, row:usize, col:usize, row_len:usize, col_len:usize, direction:&[(isize, isize)]) -> usize
+fn search_in_direction(seats:&Vec<bool>, occupied:&Vec<isize>, col:usize, row:usize, row_len:usize, col_len:usize, direction:&(isize, isize)) -> isize
 {
     let mut at_edge = false;
-    let mut direction_length = 1;
     let mut rw = row as isize;
     let mut cl = col as isize;
     while !at_edge {
-        cl += direction[0];
-        rw += direction[1];
-        if seat[idx(cl, rw, col_len)] 
+        cl += direction.0;
+        rw += direction.1;
+        if (0 <= rw && rw < row_len as isize) && (0 <= cl && cl < col_len as isize) 
         {
-            if occupied[idx(cl, rw, col_len)] == 1
+            if seats[idx(cl as usize, rw as usize, col_len)] 
             {
-                return 1;
+                if occupied[idx(cl as usize, rw as usize, col_len)] == 1
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
             }
-            else
-            {
-                return 0;
-            }
+        }
+        else
+        {
+            at_edge = true;
         }
     }
     return 0;
@@ -158,6 +172,30 @@ fn idx(col:usize, row:usize, col_len:usize)->usize
 mod tests {
     use super::*;
 
+    
+    #[test]
+    fn verify_count_neighbors_in_sieght() {
+        // Test design:
+        //  # L # L L
+        //  L . . . L
+        //  L L T L #
+        //  L L L L L
+        //  L L L L L
+        let col_len = 5;
+        let row_len = 5;
+        let mut seats = vec![true;25];
+        seats[6] = false;
+        seats[7] = false;
+        seats[8] = false;
+        let mut occupied = vec![0;25];
+        
+        occupied[0] = 1;
+        occupied[2] = 1;
+        occupied[idx(4, 2, col_len)];
+        let neighbours = count_neighbours_in_sight(&seats, &occupied, 2, 2, row_len, col_len);
+        assert_eq!(2, neighbours);
+    }
+
     #[test]
     fn verify_count_neighbors() {
         // Test design:
@@ -167,29 +205,31 @@ mod tests {
         // row length
 
         // bottom row
-        let ret_vec = count_neighbours(&vec![1;9], 2, 1, 3, 3);
-        assert_eq!(5, ret_vec);
+        let neighbours = count_neighbours(&vec![1;9], 2, 1, 3, 3);
+        assert_eq!(5, neighbours);
 
         // top row
-        let ret_vec = count_neighbours(&vec![1;9], 0, 1, 3, 3);
-        assert_eq!(5, ret_vec);
+        let neighbours = count_neighbours(&vec![1;9], 0, 1, 3, 3);
+        assert_eq!(5, neighbours);
 
         // top left corner
-        let ret_vec = count_neighbours(&vec![1;9], 0, 0, 3, 3);
-        assert_eq!(3, ret_vec);
+        let neighbours = count_neighbours(&vec![1;9], 0, 0, 3, 3);
+        assert_eq!(3, neighbours);
 
         // top left corner
-        let ret_vec = count_neighbours(&vec![1;9], 2, 2, 3, 3);
-        assert_eq!(3, ret_vec);
+        let neighbours = count_neighbours(&vec![1;9], 2, 2, 3, 3);
+        assert_eq!(3, neighbours);
 
         // Exactly in the middle
-        let ret_vec = count_neighbours(&vec![1;9], 1, 1, 3, 3);
-        assert_eq!(8, ret_vec);
+        let neighbours = count_neighbours(&vec![1;9], 1, 1, 3, 3);
+        assert_eq!(8, neighbours);
         
         // Subset of data
-        let ret_vec = count_neighbours(&vec![0, 0, 0, 0, 0, 0, 1, 1, 1], 1, 1, 3, 3);
-        assert_eq!(3, ret_vec);
+        let neighbours = count_neighbours(&vec![0, 0, 0, 0, 0, 0, 1, 1, 1], 1, 1, 3, 3);
+        assert_eq!(3, neighbours);
     }
+
+
     #[test]
     fn verify_matrix_equality() {
         // 1 2 3 
@@ -205,7 +245,7 @@ mod tests {
 
     #[test]
     fn verify_index() {
-        let mut a = vec![1, 2, 3, 4, 5, 6];
+        let a = vec![1, 2, 3, 4, 5, 6];
         let row_length = 3;
 
         assert_eq!(2, a[idx(1,0,row_length)]);
